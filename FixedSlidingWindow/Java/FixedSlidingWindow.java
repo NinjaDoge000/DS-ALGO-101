5a3d5ad4b976bb3526fabc9416ea95bae69e0e92
NinjaDoge <ninjadoge000@gmail.com>
2025-07-06 18:09:24 +0000

    Fixed sliding window

---
+++ FixedSlidingWindow/Java/FixedSlidingWindow.java
@@ -0,0 +1,23 @@
+public class FixedSlidingWindow {
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int start = 0; 
+        int windowSize = 3;
+
+        int windowSum = 0;
+        for(int end = 0; end < arr.length; end++) {
+
+            windowSum += arr[end];
+            
+            // window size exceeded
+            if(end >= windowSize) {
+                windowSum -= arr[start];
+                start++;
+            } 
+
+            System.out.println("Window sum now is " + windowSum);
+        }
+    }
+}
