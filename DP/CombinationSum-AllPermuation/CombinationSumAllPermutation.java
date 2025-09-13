0e0fd80e70eeecadab6bcb89ba61eddeddebd2bb
NinjaDoge <ninjadoge000@gmail.com>
2025-09-13 13:38:18 +0000

    combinationSum - all permutation

---
+++ DP/CombinationSum-AllPermuation/CombinationSumAllPermutation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class CombinationSumAllPermutation {
+    int[][] dp;
+    
+    int solve(int i, int sum, int[] nums, int target) {
+        if (i == nums.length) {
+            return (sum == target) ? 1 : 0;
+        }
+
+        if (dp[i][sum + 1000] != -1) return dp[i][sum + 1000];
+
+        // add nums[i]
+        int addCount = solve(i + 1, sum + nums[i], nums, target);
+        // subtract nums[i]
+        int subCount = solve(i + 1, sum - nums[i], nums, target);
+
+        return dp[i][sum + 1000] = addCount + subCount;
+    }
+
+    public int findTargetSumWays(int[] nums, int target) {
+        dp = new int[nums.length][2001]; // sum range [-1000..1000] → shifted by +1000
+        for (int[] arr : dp) Arrays.fill(arr, -1);
+
+        return solve(0, 0, nums, target);
+    }
+}
